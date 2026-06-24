95d0d0177a464d57f99957cab8e0c5898986a28b
Annu Priya <annuchy8265@gmail.com>
2026-06-24 14:31:50 +0530

    Array practice

---
+++ Array/ArraysCC.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ArraysCC {
+    public static void main (String args[]){
+        int marks[]=new int[100];
+      //  int numbers[]={1,2,3,4};
+      //  int moreNumbers[]={4,5,6};
+      Scanner sc= new Scanner(System.in);
+      
+      System.out.println("length of array="+marks.length);
+      // marks[0]=sc.nextInt();
+       //marks[1]=sc.nextInt();
+       //marks[2]=sc.nextInt();
+
+//System.out.println("phy : " + marks[0]);
+//System.out.println("chem : "+ marks[1]);
+//System.out.println("math : "+ marks[2]);
+
+//marks[2]=marks[2]+1;
+//System.out.println("math : "+ 1212marks[2]);
+
+//int percentage =(marls[0]+marks[1]+marks[2])/3;
+//System.out.println("percentage ="+percentage +"%");
+
+}
+}
