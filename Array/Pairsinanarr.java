9a48bf4b93869f93eb9e7753c9b0bc3a913a19d2
Annu Priya <annuchy8265@gmail.com>
2026-06-26 12:36:50 +0530

    arrays basic q

--- Array/Pairsinanarr.java
+++ Array/Pairsinanarr.java
@@ -4,7 +4,7 @@ public class Pairsinanarr {
         for(int i=0;i<numbers.length;i++){
             int curr = numbers[i];//2,4,6,8,10
             for(int j=i+1; j<numbers.length; j++) {
-                System.out.println("(" + curr +","+ numbers[j] + ")");
+                System.out.print("(" + curr +","+ numbers[j] + ")");
             }
         
             System.out.println();
